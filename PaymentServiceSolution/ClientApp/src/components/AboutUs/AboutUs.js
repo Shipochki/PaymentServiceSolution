1d7d32a71b52d96671e885d88ad26580f8e89b8c
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-11-08 12:06:24 +0200

    Beautifying the sections: What we offer, our vision, why choose Literary Lights

--- PaymentServiceSolution/ClientApp/src/components/AboutUs/AboutUs.js
+++ PaymentServiceSolution/ClientApp/src/components/AboutUs/AboutUs.js
@@ -18,6 +18,7 @@ export const AboutUs = () => {
             </p>
           </div>
         </div>
+        <hr />
         <div className="our-story">
           <h2>Our Story</h2>
           <div className="our-story-content">
@@ -29,85 +30,96 @@ export const AboutUs = () => {
               connect with the stories that enrich our lives.
             </p>
             <div className="our-story-img">
-              <img src='https://theanswerclass.com/wp-content/uploads/2018/02/bigstock-Happy-Group-Of-People-3928383-2-1-scaled.jpg'/>
+              <img src="https://theanswerclass.com/wp-content/uploads/2018/02/bigstock-Happy-Group-Of-People-3928383-2-1-scaled.jpg" />
             </div>
           </div>
         </div>
+        <hr />
         <div className="our-vision">
           <h2>Our Vision</h2>
-          <div className="our-vision-img">
-            <img />
+          <div className="our-vision-content">
+            <div className="our-vision-img">
+              <img src="https://ychef.files.bbci.co.uk/976x549/p03gg1lc.jpg" />
+            </div>
+            <p>
+              Literary Lights envisions a world where books are more than just
+              objects; they are beacons of knowledge, gateways to other worlds,
+              and sources of endless inspiration. We strive to kindle the love
+              of reading in people of all ages and backgrounds, fostering a
+              community that celebrates the power of storytelling.
+            </p>
           </div>
-          <p>
-            Literary Lights envisions a world where books are more than just
-            objects; they are beacons of knowledge, gateways to other worlds,
-            and sources of endless inspiration. We strive to kindle the love of
-            reading in people of all ages and backgrounds, fostering a community
-            that celebrates the power of storytelling.
-          </p>
         </div>
+        <hr />
         <div className="what-we-offer">
           <h2>What We Offer</h2>
-          <div className="what-we-offer-img">
-            <img />
+          <div className="what-we-offer-content">
+            <p>
+              We curate an extensive collection of books that span various
+              genres, from classic literature to contemporary bestsellers. Our
+              shelves are adorned with tales that provoke thought, spark
+              conversation, and transport readers to far-off places or different
+              eras. Whether you're seeking an adventure, searching for
+              knowledge, or simply looking for a great escape, you'll find it in
+              our carefully selected library.
+            </p>
+            <div className="what-we-offer-img">
+              <img src="https://images.inc.com/uploaded_files/image/1920x1080/getty_655998316_2000149920009280219_363765.jpg" />
+            </div>
           </div>
-          <p>
-            We curate an extensive collection of books that span various genres,
-            from classic literature to contemporary bestsellers. Our shelves are
-            adorned with tales that provoke thought, spark conversation, and
-            transport readers to far-off places or different eras. Whether
-            you're seeking an adventure, searching for knowledge, or simply
-            looking for a great escape, you'll find it in our carefully selected
-            library.
-          </p>
         </div>
+        <hr />
         <div className="why-choose">
           <h2>Why Choose Literary Lights</h2>
-          <div className="quality">
-            <img />
-            <p>
-              Quality Assurance: We take pride in offering high-quality books
-              that have been selected with care. You can trust that each book we
-              offer has been chosen for its content, craftsmanship, and literary
-              merit.
-            </p>
-          </div>
-          <div className="personalized ">
-            <img />
-            <p>
-              Personalized Recommendations: Our team of dedicated bibliophiles
-              is always ready to assist you in finding the perfect book for your
-              tastes and interests. We provide personalized recommendations and
-              expert advice to enhance your reading experience.
-            </p>
-          </div>
-          <div className="community ">
-            <img />
-            <p>
-              Community Engagement: At Literary Lights, we understand that books
-              are a source of connection. We host book clubs, author events, and
-              literary discussions to bring book enthusiasts together and
-              celebrate the joy of reading.
-            </p>
-          </div>
-          <div className="convenience">
-            <img />
-            <p>
-              Convenience: Our online store provides a seamless and convenient
-              shopping experience. Browse, purchase, and have your favorite
-              books delivered right to your doorstep.
-            </p>
-          </div>
-          <div className="philanthropy">
-            <img />
-            <p>
-              Philanthropy: We are committed to giving back to the literary
-              community. A portion of our proceeds goes towards supporting
-              literacy programs, libraries, and initiatives that promote the
-              love of reading.
-            </p>
+          <div className="why-choose-content">
+            <div className="quality">
+              <img src='https://www.cabem.com/wp-content/uploads/2023/03/quality-assurance-1.jpg' />
+              <p>
+                <b>Quality Assurance:</b> We take pride in offering high-quality books
+                that have been selected with care. You can trust that each book
+                we offer has been chosen for its content, craftsmanship, and
+                literary merit.
+              </p>
+            </div>
+            <div className="personalized ">
+              <img src='https://images.hiverhq.com/blog/wp-content/uploads/2017/08/tr:pr-true/Delivering-Personalized-Customer-Service.png' />
+              <p>
+                <b>Personalized Recommendations:</b> Our team of dedicated bibliophiles
+                is always ready to assist you in finding the perfect book for
+                your tastes and interests. We provide personalized
+                recommendations and expert advice to enhance your reading
+                experience.
+              </p>
+            </div>
+            <div className="community ">
+              <img src='https://www.wellesleyinstitute.com/wp-content/uploads/2021/03/iStock-1297711206.jpg'/>
+              <p>
+                <b>Community Engagement:</b> At Literary Lights, we understand that
+                books are a source of connection. We host book clubs, author
+                events, and literary discussions to bring book enthusiasts
+                together and celebrate the joy of reading.
+              </p>
+            </div>
+            <div className="convenience">
+              <img src='https://www.raconteur.net/wp-content/uploads/2017/12/customer-experience-crop.jpg'/>
+              <p>
+                <b>Convenience:</b> Our online store provides a seamless and convenient
+                shopping experience. Browse, purchase, and have your favorite
+                books delivered right to your doorstep.
+              </p>
+            </div>
+            <div className="philanthropy">
+              <img src='https://media.newyorker.com/photos/59097b3b019dfc3494ea3675/master/pass/Vara-Charity.jpg'/>
+              <p>
+                <b>Philanthropy:</b> We are committed to giving back to the literary
+                community. A portion of our proceeds goes towards supporting
+                literacy programs, libraries, and initiatives that promote the
+                love of reading.
+              </p>
+            </div>
           </div>
         </div>
+        <hr />
         <div className="join-our-journey">
           <h2>Join Our Journey</h2>
           <div className="join-our-img">
@@ -120,6 +132,7 @@ export const AboutUs = () => {
             burning brightly for generations to come.
           </p>
         </div>
+
         <p>
           Thank you for choosing Literary Lights as your source for literary
           treasures.
