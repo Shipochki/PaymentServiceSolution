f14a6c1178c4a6cf1506dfe8b3f5e3ce9b0fb5e2
Shipochki <121862582+Shipochki@users.noreply.github.com>
2023-11-08 13:06:04 +0200

    Established footer and beautified sections

--- PaymentServiceSolution/ClientApp/src/components/AboutUs/AboutUs.js
+++ PaymentServiceSolution/ClientApp/src/components/AboutUs/AboutUs.js
@@ -73,48 +73,48 @@ export const AboutUs = () => {
           <h2>Why Choose Literary Lights</h2>
           <div className="why-choose-content">
             <div className="quality">
-              <img src='https://www.cabem.com/wp-content/uploads/2023/03/quality-assurance-1.jpg' />
+              <img src="https://www.cabem.com/wp-content/uploads/2023/03/quality-assurance-1.jpg" />
               <p>
-                <b>Quality Assurance:</b> We take pride in offering high-quality books
-                that have been selected with care. You can trust that each book
-                we offer has been chosen for its content, craftsmanship, and
-                literary merit.
+                <b>Quality Assurance:</b> We take pride in offering high-quality
+                books that have been selected with care. You can trust that each
+                book we offer has been chosen for its content, craftsmanship,
+                and literary merit.
               </p>
             </div>
             <div className="personalized ">
-              <img src='https://images.hiverhq.com/blog/wp-content/uploads/2017/08/tr:pr-true/Delivering-Personalized-Customer-Service.png' />
+              <img src="https://images.hiverhq.com/blog/wp-content/uploads/2017/08/tr:pr-true/Delivering-Personalized-Customer-Service.png" />
               <p>
-                <b>Personalized Recommendations:</b> Our team of dedicated bibliophiles
-                is always ready to assist you in finding the perfect book for
-                your tastes and interests. We provide personalized
-                recommendations and expert advice to enhance your reading
-                experience.
+                <b>Personalized Recommendations:</b> Our team of dedicated
+                bibliophiles is always ready to assist you in finding the
+                perfect book for your tastes and interests. We provide
+                personalized recommendations and expert advice to enhance your
+                reading experience.
               </p>
             </div>
             <div className="community ">
-              <img src='https://www.wellesleyinstitute.com/wp-content/uploads/2021/03/iStock-1297711206.jpg'/>
+              <img src="https://www.wellesleyinstitute.com/wp-content/uploads/2021/03/iStock-1297711206.jpg" />
               <p>
-                <b>Community Engagement:</b> At Literary Lights, we understand that
-                books are a source of connection. We host book clubs, author
-                events, and literary discussions to bring book enthusiasts
-                together and celebrate the joy of reading.
+                <b>Community Engagement:</b> At Literary Lights, we understand
+                that books are a source of connection. We host book clubs,
+                author events, and literary discussions to bring book
+                enthusiasts together and celebrate the joy of reading.
               </p>
             </div>
             <div className="convenience">
-              <img src='https://www.raconteur.net/wp-content/uploads/2017/12/customer-experience-crop.jpg'/>
+              <img src="https://www.raconteur.net/wp-content/uploads/2017/12/customer-experience-crop.jpg" />
               <p>
-                <b>Convenience:</b> Our online store provides a seamless and convenient
-                shopping experience. Browse, purchase, and have your favorite
-                books delivered right to your doorstep.
+                <b>Convenience:</b> Our online store provides a seamless and
+                convenient shopping experience. Browse, purchase, and have your
+                favorite books delivered right to your doorstep.
               </p>
             </div>
             <div className="philanthropy">
-              <img src='https://media.newyorker.com/photos/59097b3b019dfc3494ea3675/master/pass/Vara-Charity.jpg'/>
+              <img src="https://media.newyorker.com/photos/59097b3b019dfc3494ea3675/master/pass/Vara-Charity.jpg" />
               <p>
-                <b>Philanthropy:</b> We are committed to giving back to the literary
-                community. A portion of our proceeds goes towards supporting
-                literacy programs, libraries, and initiatives that promote the
-                love of reading.
+                <b>Philanthropy:</b> We are committed to giving back to the
+                literary community. A portion of our proceeds goes towards
+                supporting literacy programs, libraries, and initiatives that
+                promote the love of reading.
               </p>
             </div>
           </div>
@@ -122,18 +122,20 @@ export const AboutUs = () => {
         <hr />
         <div className="join-our-journey">
           <h2>Join Our Journey</h2>
-          <div className="join-our-img">
-            <img />
+          <div className="join-our-content">
+            <div className="join-our-img">
+              <img src="https://www.beinspirational.co.uk/wp-content/uploads/2021/03/userjourneymain.jpg" />
+            </div>
+            <p>
+              We invite you to be a part of the Literary Lights community.
+              Explore our collection, embark on literary adventures, and share
+              your love for books with us. Together, we can keep the flame of
+              literature burning brightly for generations to come.
+            </p>
           </div>
-          <p>
-            We invite you to be a part of the Literary Lights community. Explore
-            our collection, embark on literary adventures, and share your love
-            for books with us. Together, we can keep the flame of literature
-            burning brightly for generations to come.
-          </p>
         </div>
-
-        <p>
+        <hr />
+        <p className='thank-you'>
           Thank you for choosing Literary Lights as your source for literary
           treasures.
         </p>
--- PaymentServiceSolution/ClientApp/src/components/Footer/Footer.js
+++ PaymentServiceSolution/ClientApp/src/components/Footer/Footer.js
@@ -1,7 +1,30 @@
-export const Footer = () => {
-    return(
-        <footer>
+import './footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
-        </footer>
-    )
+export const Footer = () => {
+    return (
+      <footer>
+        <div className='footer-content'>
+        <div className="get-know-us">
+            <p>Get know us</p>
+            <Link to={'/aboutus'}>About Us</Link>
+        </div>
+        <div className="payment">
+            <p>Payment partner</p>
+            <a href='https://stripe.com/'>Stripe payment</a>
+            <a href='https://stripe.com/en-gb-bg/payments'>Payment Info</a>
+        </div>
+        <div className="contacts">
+            <p>Contacts:</p>
+          <FontAwesomeIcon icon={faFacebookF} />
+          <FontAwesomeIcon icon={faInstagram} />
+          <FontAwesomeIcon icon={faYoutube} />
+          <FontAwesomeIcon icon={faLinkedin} />
+        </div>
+        </div>
+        <p>© 2002-2023, LiteratyLights.com, Inc. or its affiliates</p>
+      </footer>
+    );
 }
